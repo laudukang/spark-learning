ada755188509761da9c900834b81a21cfb9738df
laudukang <751611201@qq.com>
2017-03-08 16:45:57 +0800

    restructure Jedis

--- src/main/java/me/codz/redis/RedisJava.java
+++ src/main/java/me/codz/redis/RedisJava.java
@@ -1,5 +1,6 @@
 package me.codz.redis;
 
+import com.google.common.collect.Lists;
 import me.codz.beans.Blog;
 import org.apache.commons.lang3.SerializationUtils;
 import redis.clients.jedis.Jedis;
@@ -16,18 +17,29 @@ import java.util.Set;
  * <p>Version: 1.0
  */
 public class RedisJava {
+	private static Jedis jedis;
+
+	static {
+		//jedis.auth("tmp");
+		jedis = new Jedis("192.168.1.10", 6379);
+		System.out.println("Server is running: " + jedis.ping());
+	}
 
 	public static void main(String[] args) {
-		Jedis jedis = new Jedis("192.168.1.10", 6379);
-		//jedis.auth("tmp");
-		System.out.println("Server is running: " + jedis.ping());
+		//stringTest();
+		//listTest();
+		//keysTest();
+		//beanTest();
+		beanListTest();
+	}
 
-		//String test
+	private static void stringTest() {
 		System.out.println("\nString test:");
 		jedis.set("hi", "lau");
 		System.out.println(jedis.get("hi"));
+	}
 
-		//List test
+	private static void listTest() {
 		System.out.println("\nList test:");
 		jedis.del("tutorial-list");
 		jedis.lpush("tutorial-list", "Redis");
@@ -35,13 +47,15 @@ public class RedisJava {
 		jedis.lpush("tutorial-list", "Mysql");
 		List<String> list = jedis.lrange("tutorial-list", 0, -1);
 		list.forEach(System.out::println);
+	}
 
-		//Keys test
+	private static void keysTest() {
 		System.out.println("\nKey set:");
 		Set<String> keySet = jedis.keys("*");
 		keySet.forEach(System.out::println);
+	}
 
-		//Bean test
+	private static void beanTest() {
 		Blog blog = new Blog();
 		blog.setTitle("Spark is running");
 		blog.setContent("Awesome spark");
@@ -53,6 +67,37 @@ public class RedisJava {
 
 		Blog blogFromRedis = SerializationUtils.deserialize(jedis.get(blogCacheKey.getBytes()));
 		System.out.println(blogFromRedis);
+	}
 
+	private static void beanListTest() {
+		Blog blog = new Blog();
+		blog.setTitle("Awesome spark");
+		blog.setContent("Spark is running");
+		blog.setAuthor("apache");
+		blog.setCreateTime(new Date());
+
+		Blog blog2 = new Blog();
+		blog2.setTitle("Redis Desktop Manager");
+		blog2.setContent("Open source cross-platform Redis Desktop Manager based on Qt 5");
+		blog2.setAuthor("user");
+		blog2.setCreateTime(new Date());
+
+		List<byte[]> blogList = Lists.newArrayList();
+		blogList.add(SerializationUtils.serialize(blog));
+		blogList.add(SerializationUtils.serialize(blog2));
+
+		String blogListCacheKey = "bloglist";
+		if (jedis.exists(blogListCacheKey.getBytes())) {
+			jedis.del(blogListCacheKey.getBytes());
+		}
+		byte[][] data = blogList.toArray(new byte[blogList.size()][]);
+
+		jedis.rpush(blogListCacheKey.getBytes(), data);
+
+		List<byte[]> blogByteFromRedis = jedis.lrange(blogListCacheKey.getBytes(), 0, -1);
+		blogByteFromRedis.forEach(b -> {
+			Object object = SerializationUtils.deserialize(b);
+			System.out.println(object);
+		});
 	}
 }
